c59582a605d6427e487334b6966cc35be0b3782b
Irini-tsitse <tsitse@eshcc.eur.nl>
2024-06-21 14:55:51 +0200

    Upload sub and training

---
+++ submission.py
@@ -0,0 +1,64 @@
+"""
+This is an example script to generate the outcome variable given the input dataset.
+
+This script should be modified to prepare your own submission that predicts 
+the outcome for the benchmark challenge by changing the clean_df and predict_outcomes function.
+
+The predict_outcomes function takes a Pandas data frame. The return value must
+be a data frame with two columns: nomem_encr and outcome. The nomem_encr column
+should contain the nomem_encr column from the input data frame. The outcome
+column should contain the predicted outcome for each nomem_encr. The outcome
+should be 0 (no child) or 1 (having a child).
+
+clean_df should be used to clean (preprocess) the data.
+
+run.py can be used to test your submission.
+"""
+
+# List your libraries and modules here. Don't forget to update environment.yml!
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+
+def clean_df(df, background_df=None):
+    y_missing = mydata['outcome_available'] == 0
+    mydata = mydata.drop(mydata[y_missing].index, axis='rows')
+#outcome = outcome.drop(outcome[y_missing].index, axis='rows')
+    selected_columns = ["cf20m024","cf20m128","cf20m180","ci20m006","ci20m261","nomem_encr"]
+    features = mydata[selected_columns]
+    X_isna = features.isna().any(axis=1)
+    features = features.drop(features[X_isna].index)
+
+    return features
+
+clean_df(mydata)
+cleaned_data = clean_df(mydata)
+
+from sklearn.linear_model import LogisticRegression
+train_save_model(cleaned_data, outcome)
+
+def predict_outcomes(df, background_df=None, model_path="model.joblib"):
+  ## This script contains a bare minimum working example
+    if "nomem_encr" not in df.columns:
+        print("The identifier variable 'nomem_encr' should be in the dataset")
+
+    # Load the model
+    model = joblib.load(model_path)
+
+    # Preprocess the fake / holdout data
+    df = clean_df(df, background_df)
+
+    # Exclude the variable nomem_encr if this variable is NOT in your model
+    vars_without_id = df.columns[df.columns != 'nomem_encr']
+
+    # Generate predictions from model, should be 0 (no child) or 1 (had child)
+    predictions = model.predict(df[vars_without_id])
+
+    # Output file should be DataFrame with two columns, nomem_encr and predictions
+    df_predict = pd.DataFrame(
+        {"nomem_encr": df["nomem_encr"], "prediction": predictions}
+    )
+
+    # Return only dataset with predictions and identifier
+    return df_predict
---
+++ training.py
@@ -0,0 +1,27 @@
+"""
+This is an example script to train your model given the (cleaned) input dataset.
+
+This script will not be run on the holdout data, 
+but the resulting model model.joblib will be applied to the holdout data.
+
+It is important to document your training steps here, including seed, 
+number of folds, model, et cetera
+"""
+
+def train_save_model(cleaned_df, outcome_df):
+ ## This script contains a bare minimum working example
+    
+    # Combine cleaned_df and outcome_df
+    model_df = pd.merge(cleaned_df, outcome_df, on="nomem_encr")
+
+    # Filter cases for whom the outcome is not available
+    model_df = model_df[~model_df['new_child'].isna()]  
+    
+    # Logistic regression model
+    model = LogisticRegression()
+
+    # Fit the model
+    model.fit(model_df[["cf20m024","cf20m128","cf20m180","ci20m006","ci20m261"]], model_df['new_child'])
+
+    # Save the model
+    joblib.dump(model, "model.joblib")
